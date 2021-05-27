c919146950c7da5b273b30772597e6c01766724a
MateuszTk <41924838+MateuszTk@users.noreply.github.com>
2021-05-27 21:53:02 +0200

    Update PP0602B-Tabelki_liczb.cpp

--- PP0602B-Tabelki_liczb.cpp
+++ PP0602B-Tabelki_liczb.cpp
@@ -6,61 +6,91 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t-- > 0) {
-        int l, k;
-        cin >> l >> k;//y x
-        int* arr = new int[l * k];
-        for (int y = 0; y < l; y++) {
-            for (int x = 0; x < k; x++) {
-                cin >> arr[index(x, y, k)];
-            }
-        }
+	int t;
+	cin >> t;
+	while (t-- > 0) {
+		int l, k;
+		cin >> l >> k; //y x
+		int* arr = new int[l * k];
+		for (int y = 0; y < l; y++) {
+			for (int x = 0; x < k; x++) {
+				cin >> arr[index(x, y, k)];
+			}
+		}
 
-        for (int y = 0; y < l; y++) {
-            for (int x = 0; x < k; x++) {
-                int xo = x, yo = y;
-                /*if (yo < (float)((l - 1) * 0.5f)) {
-                    if(xo + 1 < k)
-                        xo++; 
-                }
-                else if (yo > (float)((l - 1) * 0.5f)) {
-                    if (xo > 0)
-                        xo--; 
-                }*/
+		//cout << '\n';
+		float centx = (k - 1) * 0.5f;
+		for (int y = 0; y < l; y++) {
+			for (int x = 0; x < k; x++) {
+				int xo = x, yo = y;
+				int st = (yo > (float)((l - 1) * 0.5f)) ? (l - 1 - y) : y;
+				if (yo < (float)((l - 1) * 0.5f)) {
+					if (xo + 1 < k)
+						xo++;
+				}
+				else if (yo > (float)((l - 1) * 0.5f)) {
+					if (xo > 0)
+						xo--;
+				}
 
-                if (x == 0 && y > 0) {
-                    xo = 0;
-                    yo--;
-                }
-                if (x == k - 1 && y < l - 1) {
-                    xo = k - 1;
-                    yo++;
-                }
-                if (y == l - 1 && x > 0) {
-                    xo--;
-                    yo = l - 1;
-                }
-                if (y == 0 && x < k - 1) {
-                    yo = 0;
-                    xo++;
-                }
+				if (x <= st && x != y) {
+					xo = x;
+					yo--;
+				}
+				if (x >= k - st - 1 && y != l - (k - x)) {
+					xo = x;
+					yo++;
+				}
 
+				if (centx == (float)x && y >= centx && y < l - centx) {
+					xo = x;
+					yo = y;
+				}
 
-                cout << arr[index(xo, yo, k)] << ' ';
-            }
-            cout << '\n';
-        }
+				cout << arr[index(xo, yo, k)] << ' ';
+			}
+			cout << '\n';
+		}
 
-
-        delete[] arr;
-    }
-    return 0;
+		delete[] arr;
+	}
+	return 0;
 }
+
+//tests
+
 /*1
 4 4
 1 2 3 4
 5 6 7 8
 9 10 11 12
 13 14 15 16*/
+
+/*
+1
+5 3
+1 2 3
+4 5 6
+7 8 9
+10 11 12
+13 14 15
+*/
+
+/*
+1
+5 4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+17 18 19 20
+*/
+
+/*
+1
+4 5
+0 1 2 3 4
+5 6 7 8 9
+10 11 12 13 14
+15 16 17 18 19
+*/
